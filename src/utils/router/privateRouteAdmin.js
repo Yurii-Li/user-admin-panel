f6469566399127d396490e22835db4bc13194d3d
Yuriy Lyubuskyi <liubuskiy.yuri@gmail.com>
2023-03-05 01:37:53 +0200

    fix

--- src/utils/router/privateRouteAdmin.js
+++ src/utils/router/privateRouteAdmin.js
@@ -1,11 +1,28 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {Outlet} from "react-router-dom";
+import {useEffect} from "react";
+
 import {Page404} from "../../pages";
+import {adminProfileActions} from "../../redux/slices";
+
 
 
 const PrivateRouteAdmin = () => {
 
-    const {adminProfile} = useSelector((state) => state.adminProfileReducer);
+    const dispatch = useDispatch();
+    const {adminProfile, loading} = useSelector((state) => state.adminProfileReducer);
+
+    useEffect(() => {
+        if (!adminProfile ) {
+            dispatch(adminProfileActions.getAdminProfile());
+        }
+    }, [dispatch, adminProfile]);
+
+
+    if (loading) {
+        return null;
+    }
+
 
     return (
         adminProfile?.is_superuser ? <Outlet /> : <Page404 />

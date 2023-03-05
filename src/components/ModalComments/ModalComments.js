a512439c8cc979aac97552f357a862d53d14a087
Yuriy Lyubuskyi <liubuskiy.yuri@gmail.com>
2023-03-05 19:59:54 +0200

    stage 3

--- src/components/ModalComments/ModalComments.js
+++ src/components/ModalComments/ModalComments.js
@@ -1,11 +1,36 @@
 import "./modalComments.scss";
 import {ModalComment} from "../ModalComment/ModalComment";
+import {useMemo, useState} from "react";
+import {Pagination} from "../Pagination/Pagination";
+
+let PageSize = 5;
 
 const ModalComments = ({ comments, setOpenModalComments }) => {
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const currentComments = useMemo(()=>{
+        const firstPageIndex = (currentPage - 1) * PageSize;
+        const lastPageIndex = firstPageIndex + PageSize;
+        return comments.slice(firstPageIndex, lastPageIndex);
+    }, [currentPage]);
+
     return (
         <div className={"modal-comments"}>
-            <div>{comments.length === 0 ? "null" : comments.map((item) => <ModalComment key={item.id} item={item} />).reverse()}</div>
-            <button className={"modal-comments__button"} onClick={() => setOpenModalComments(false)}>Close</button>
+            <div>{comments.length === 0 ? "null" :
+                currentComments.map((item, index) => {
+                    return <ModalComment key={index} item={item}/>
+                })
+            }
+
+            </div>
+
+            <div className={"modal-comments__controls"}>
+                <div className={"modal-comments__pagination"}>
+                    <Pagination currentPage={currentPage} totalCount={comments.length} pageSize={PageSize} onPageChange={page => setCurrentPage(page)}/>
+                </div>
+                <button className={"modal-comments__button"} onClick={() => setOpenModalComments(false)}>Close</button>
+            </div>
 
         </div>
     );
